2b6f8b268654ae94d8405b34ad4cec8930d01467
yagobelo <yago.belo00@gmail.com>
2023-08-05 14:05:33 -0300

    feat: add Chat in pages and add animations in Chat

--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -1,14 +1,102 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import * as Animatable from "react-native-animatable";
 
 export default function Chat() {
   return (
-    <View>
-      <Text>Chat</Text>
+    <View style={styles.container}>
+      <Animatable.View
+        animation={"fadeInLeft"}
+        deley={1000}
+        style={styles.containerHeader}
+      >
+        <Text style={styles.textHeader}>Olá, Usuário!</Text>
+      </Animatable.View>
+
+      <Animatable.View
+        animation={"zoomIn"}
+        delay={1000}
+        style={styles.containerChat}
+      ></Animatable.View>
+
+      <Animatable.View
+        animation={"zoomInDown"}
+        deley={1000}
+        style={styles.containerForm}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder="Digite sua mensagem aqui..."
+          placeholderTextColor={"white"}
+        ></TextInput>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.textButton}>Enviar</Text>
+        </TouchableOpacity>
+      </Animatable.View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    backgroundColor: "#264653",
+  },
+  containerHeader: {
+    paddingTop: 50,
+    marginStart: 20,
+  },
+  textHeader: {
+    fontSize: 30,
+    color: "white",
+    fontWeight: "bold",
+  },
+  containerChat: {
+    backgroundColor: "white",
+    width: "90%",
+    height: "80%",
+    alignSelf: "center",
+    padding: 20,
+    marginTop: 10,
+    borderRadius: 15,
+    flex: 2,
+  },
+  containerForm: {
+    backgroundColor: "white",
+    width: "95%",
+    margin: 5,
+    borderRadius: 15,
+    flexDirection: "row",
+    padding: 10,
+    alignSelf: "center",
+  },
+  input: {
+    backgroundColor: "grey",
+    width: "85%",
+    height: 40,
+    borderTopLeftRadius: 15,
+    borderBottomLeftRadius: 15,
+    color: "white",
+    fontSize: 20,
+    padding: 5,
+  },
+  button: {
+    backgroundColor: "#06d6a0",
+    width: "15%",
+    borderTopRightRadius: 15,
+    borderBottomRightRadius: 15,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textButton: {
+    color: "#264653",
+    fontSize: 15,
+    fontWeight: "bold",
+  },
 });
